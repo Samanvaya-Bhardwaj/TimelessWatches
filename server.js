1932c6137b5b2e9925d8c4ebbec4667e8bc2363f
Samanvaya Bhardwaj <samanvayabhardwaj2001@gmail.com>
2024-04-13 14:36:43 +0530

    Deployment Changes

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 // database connection
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
@@ -23,6 +28,7 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 
@@ -30,10 +36,11 @@ app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product', productRoutes)
 
+
 // api
 
-app.get('/', (req, res) => {
-      res.send("<h1>Welcome to our app</h1>")
+app.use('*', function(req, res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 const PORT = process.env.PORT || 8000; 
